ec5677288712df88a6b3e3db7df5b3b26b2b3857
Tonnerre Lombard <tonnerre@thebsh.sygroup.ch>
2008-03-07 15:31:08 +0000

    Adapt .PL registration periods to the documentation

--- lib/Net/DRI/DRD/PL.pm
+++ lib/Net/DRI/DRD/PL.pm
@@ -79,7 +79,7 @@ sub new
  return $self;
 }
 
-sub periods  { return map { DateTime::Duration->new(years => $_) } (1); }
+sub periods  { return map { DateTime::Duration->new(years => $_) } (1..10); }
 sub name     { return 'NASK'; }
 sub tlds     { return ('pl'); }
 sub object_types { return ('domain','contact','ns'); }

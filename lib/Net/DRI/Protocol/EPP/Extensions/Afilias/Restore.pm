76eb6ffde7e912bf430098ea7c3017d6b07cbd1d
Tonnerre Lombard <tonnerre@thebsh.sygroup.ch>
2008-03-07 15:14:31 +0000

    Add Afilias restore extension and move PIR IDN extension to Afilias namespace

--- lib/Net/DRI/Protocol/EPP/Extensions/Afilias/Restore.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/Afilias/Restore.pm
@@ -1,6 +1,7 @@
-## Domain Registry Interface, PIR EPP extensions
+## Domain Registry Interface, EPP Renew Redemption Period Extension
 ##
-## Copyright (c) 2007 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>. All rights reserved.
+## Copyright (c) 2008 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
+##                    All rights reserved.
 ##
 ## This file is part of Net::DRI
 ##
@@ -15,19 +16,21 @@
 #
 ####################################################################################################
 
-package Net::DRI::Protocol::EPP::Extensions::PIR;
+package Net::DRI::Protocol::EPP::Extensions::Afilias::Restore;
 
 use strict;
 
-use base qw/Net::DRI::Protocol::EPP/;
+use Net::DRI::Util;
+use Net::DRI::Exception;
 
-our $VERSION=do { my @r=(q$Revision: 1.1 $=~/\d+/g); sprintf("%d".".%02d" x $#r, @r); };
+our $VERSION=do { my @r=(q$Revision: 1.3 $=~/\d+/g); sprintf("%d".".%02d" x $#r, @r); };
 
 =pod
 
 =head1 NAME
 
-Net::DRI::Protocol::EPP::Extensions::PIR - PIR (.ORG) EPP extensions for Net::DRI
+Net::DRI::Protocol::EPP::Extensions::Afilias::Restore - EPP renew redemption
+	period support for Net::DRI
 
 =head1 DESCRIPTION
 
@@ -37,22 +40,21 @@ Please see the README file for details.
 
 For now, support questions should be sent to:
 
-E<lt>netdri@dotandco.comE<gt>
+E<lt>tonnerre.lombard@sygroup.chE<gt>
 
 Please also see the SUPPORT file in the distribution.
 
 =head1 SEE ALSO
 
-E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt> and
 E<lt>http://oss.bsdprojects.net/projects/netdri/E<gt>
 
 =head1 AUTHOR
 
-Tonnerre Lombard E<lt>tonnerre.lombard@sygroup.chE<gt>
+Tonnerre Lombard, E<lt>tonnerre.lombard@sygroup.chE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2007 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
+Copyright (c) 2008 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -64,27 +66,34 @@ See the LICENSE file that comes with this distribution for more details.
 
 =cut
 
+
 ####################################################################################################
 
-sub new
+sub register_commands
 {
- my $c=shift;
- my ($drd,$version,$extrah,$defproduct)=@_;
- my %e=map { $_ => 1 } (defined($extrah)? (ref($extrah)? @$extrah : ($extrah)) : ());
+ my ($class, $version) = @_;
+ my %tmp = (
+           renew => [ \&renew, undef ],
+         );
 
- ## We do no load automatically IDNLanguage as we do not know if it is
- ## mandatory
- if (exists($e{':full'})) ## useful shortcut, modeled after Perl itself
- {
-  delete($e{':full'});
-  $e{'Net::DRI::Protocol::EPP::Extensions::PIR::IDNLanguage'}=1;
-  $e{'Net::DRI::Protocol::EPP::Extensions::GracePeriod'}=1;
- }
+ return { 'domain' => \%tmp };
+}
 
- my $self=$c->SUPER::new($drd,$version,[keys(%e)]); ## we are now officially a Net::DRI::Protocol::EPP object
+####################################################################################################
 
- bless($self,$c); ## rebless
- return $self;
+############ Transform commands
+
+sub renew
+{
+ my ($epp, $domain, $rd) = @_;
+ my $mes = $epp->message();
+ my $comment;
+
+ return unless (defined($rd->{rgp}) && $rd->{rgp});
+
+ my $eid = $mes->command_extension_register('rgp:renew',
+	'xmlns:rgp="urn:EPP:xml:ns:ext:rgp-1.0" xsi:schemaLocation="urn:EPP:xml:ns:ext:rgp-1.0 rgp-1.0.xsd"');
+ $mes->command_extension($eid, ['rgp:restore']);
 }
 
 ####################################################################################################

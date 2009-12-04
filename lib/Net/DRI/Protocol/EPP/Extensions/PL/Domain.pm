119368b997501274b7cc90c058e644c9578e65e9
Tonnerre Lombard <tonnerre.lombard@sygroup.ch>
2009-12-04 15:00:11 +0100

    Fix support for name servers in Poland

--- lib/Net/DRI/Protocol/EPP/Extensions/PL/Domain.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/PL/Domain.pm
@@ -67,7 +67,9 @@ sub register_commands
 {
  my ($class,$version)=@_;
  my %tmp=( 
-          create => [ \&create, undef ],
+          create =>	[ \&create ],
+          update =>	[ \&update ],
+	  info =>	[ undef, \&info_parse ]
          );
 
  return { 'domain' => \%tmp };
@@ -83,10 +85,56 @@ sub build_command_extension
  return $mes->command_extension_register($tag,sprintf('xmlns:extdom="%s" xsi:schemaLocation="%s %s"',$ns[0],$ns[0],$ns[1]));
 }
 
+sub build_ns
+{
+ my ($epp,$ns,$domain,$xmlns)=@_;
+ $xmlns='domain' unless defined($xmlns);
+
+ my @d;
+ @d=map { [$xmlns . ':ns',$_] } $ns->get_names();
+}
+
 sub create
 {
  my ($epp,$domain,$rd)=@_;
  my $mes=$epp->message();
+ my @d=Net::DRI::Protocol::EPP::Core::Domain::build_command($mes,'create',$domain);
+ my $def = $epp->default_parameters();
+
+ if ($def && (ref($def) eq 'HASH') && exists($def->{domain_create}) && (ref($def->{domain_create}) eq 'HASH'))
+ {
+  $rd={} unless ($rd && (ref($rd) eq 'HASH') && keys(%$rd));
+  while(my ($k,$v)=each(%{$def->{domain_create}}))
+  {
+   next if exists($rd->{$k});
+   $rd->{$k}=$v
+  }
+ }
+
+ ## Period, OPTIONAL
+ if (Net::DRI::Protocol::EPP::Core::Domain::verify_rd($rd,'duration'))
+ {
+  my $period=$rd->{duration};
+  Net::DRI::Util::check_isa($period,'DateTime::Duration');
+  push @d,Net::DRI::Protocol::EPP::Core::Domain::build_period($period);
+ }
+
+ ## Nameservers, OPTIONAL
+ push @d,build_ns($epp,$rd->{ns},$domain) if (Net::DRI::Protocol::EPP::Core::Domain::verify_rd($rd,'ns') && UNIVERSAL::isa($rd->{ns},'Net::DRI::Data::Hosts') && !$rd->{ns}->is_empty());
+
+ ## Contacts, all OPTIONAL
+ if (Net::DRI::Protocol::EPP::Core::Domain::verify_rd($rd,'contact') && UNIVERSAL::isa($rd->{contact},'Net::DRI::Data::ContactSet'))
+ {
+  my $cs=$rd->{contact};
+  my @o=$cs->get('registrant');
+  push @d,['domain:registrant',$o[0]->srid()] if (@o);
+  push @d,Net::DRI::Protocol::EPP::Core::Domain::build_contact_noregistrant($epp,$cs);
+ }
+
+ ## AuthInfo
+ Net::DRI::Exception::usererr_insufficient_parameters("authInfo is mandatory") unless (Net::DRI::Protocol::EPP::Core::Domain::verify_rd($rd,'auth') && (ref($rd->{auth}) eq 'HASH'));
+ push @d,Net::DRI::Protocol::EPP::Core::Domain::build_authinfo($rd->{auth});
+ $mes->command_body(\@d);
 
  return unless exists($rd->{reason}) || exists($rd->{book});
 
@@ -97,6 +145,81 @@ sub create
  push @e,['extdom:book']                 if (exists($rd->{book}) && $rd->{book});
 
  $mes->command_extension($eid,\@e);
+}
+
+sub update
+{
+ my ($epp,$domain,$todo)=@_;
+ my $mes=$epp->message();
+
+ Net::DRI::Exception::usererr_invalid_parameters($todo.' must be a Net::DRI::Data::Changes object') unless ($todo && UNIVERSAL::isa($todo,'Net::DRI::Data::Changes'));
+
+ if ((grep { ! /^(?:add|del)$/ } $todo->types('ns')) ||
+     (grep { ! /^(?:add|del)$/ } $todo->types('status')) ||
+     (grep { ! /^(?:add|del)$/ } $todo->types('contact')) ||
+     (grep { ! /^set$/ } $todo->types('registrant')) ||
+     (grep { ! /^set$/ } $todo->types('auth'))
+    )
+ {
+  Net::DRI::Exception->die(0,'protocol/EPP',11,'Only ns/status/contact add/del or registrant/authinfo set available for domain');
+ }
+
+ my @d=Net::DRI::Protocol::EPP::Core::Domain::build_command($mes,'update',$domain);
+
+ my $nsadd=$todo->add('ns');
+ my $nsdel=$todo->del('ns');
+ my $sadd=$todo->add('status');
+ my $sdel=$todo->del('status');
+ my $cadd=$todo->add('contact');
+ my $cdel=$todo->del('contact');
+ my (@add,@del);
+
+ push @add,build_ns($epp,$nsadd,$domain)		if $nsadd && !$nsadd->is_empty();
+ push @add,Net::DRI::Protocol::EPP::Core::Domain::build_contact_noregistrant($epp,$cadd) if $cadd;
+ push @add,$sadd->build_xml('domain:status','core')	if $sadd;
+ push @del,build_ns($epp,$nsdel,$domain)		if $nsdel && !$nsdel->is_empty();
+ push @del,Net::DRI::Protocol::EPP::Core::Domain::build_contact_noregistrant($epp,$cdel) if $cdel;
+ push @del,$sdel->build_xml('domain:status','core')	if $sdel;
+
+ push @d,['domain:add',@add] if @add;
+ push @d,['domain:rem',@del] if @del;
+
+ my $chg=$todo->set('registrant');
+ my @chg;
+ push @chg,['domain:registrant',$chg->srid()] if ($chg && ref($chg) && UNIVERSAL::can($chg,'srid'));
+ $chg=$todo->set('auth');
+ push @chg,Net::DRI::Protocol::EPP::Core::Domain::build_authinfo($chg) if ($chg && ref($chg));
+ push @d,['domain:chg',@chg] if @chg;
+ $mes->command_body(\@d);
+}
+
+sub info_parse
+{
+ my ($po, $otype, $oaction, $oname, $rinfo) = @_;
+ my $mes = $po->message();
+ return unless $mes->is_success();
+ my $infdata = $mes->get_content('infData', $mes->ns('domain'));
+ return unless $infdata;
+ my $ns = Net::DRI::Data::Hosts->new();
+ my $c = $infdata->getFirstChild();
+
+ while ($c)
+ {
+  next unless ($c->nodeType() == 1); ## only for element nodes
+  my $name = $c->localname() || $c->nodeName();
+  next unless $name;
+
+  if ($name eq 'name')
+  {
+   $oname = lc($c->getFirstChild()->getData());
+  }
+  elsif ($name eq 'ns')
+  {
+   $ns->add($c->getFirstChild()->getData());
+  }
+ } continue { $c = $c->getNextSibling(); }
+
+ $rinfo->{domain}->{$oname}->{ns} = $ns;
 }
 
 ####################################################################################################
